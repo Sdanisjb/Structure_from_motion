2bbcec04405a9843fa15f55691de90546642ea91
Sandro Caceres <sandro.caceres.olave@gmail.com>
2024-09-11 16:01:55 -0500

    Construccion de Matriz Afin, eliminar imagen poco apta

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ Clase Imagen
 
 Almacena el nombre de la imagen, keypoints, descriptores y un diccionario con los emparejamientos que tiene.
 """
+
+
 class Image:
     def __init__(self, img_name, image, keypoints, descriptors):
         self.img_name = img_name
@@ -22,31 +24,39 @@ class Image:
 
     def __hash__(self):
         return hash(self.img_name)
-    
+
     def __str__(self):
         return f"Imagen: {self.img_name} \n # Keypoints: {len(self.keypoints)} \n # Descriptores: {self.descriptors.shape} \n Matches: {list(self.matches.keys())} \n"
+
 
 """
 Clase ImageFeature
 Almacena la referencia a una imagen (nombre del archivo) junto con un índice del keypoint específico a esa imagen
 """
 
-class ImageFeature():
+
+class ImageFeature:
     def __init__(self, img_name, kpt_idx):
         self.img_name = img_name
         self.kpt_idx = kpt_idx
-    
+
     def __hash__(self):
         return hash(self.img_name + str(self.kpt_idx))
 
     def __str__(self):
         return f"{self.img_name}, keypoint: {self.kpt_idx}"
-    
+
 
 PAIR_MATCH_SURVIVAL_RATE = 0.5
 
-parser = argparse.ArgumentParser(description='Codigo para realizar SfM (Structure from Motion)')
-parser.add_argument('--images_folder', help='Path de la carpeta de imágenes', default='./images')
+parser = argparse.ArgumentParser(
+    description="Codigo para realizar SfM (Structure from Motion)"
+)
+parser.add_argument(
+    "--images_folder",
+    help="Path de la carpeta de imágenes",
+    default="./images",
+)
 args = parser.parse_args()
 
 # Diccionario de imágenes
@@ -71,7 +81,7 @@ for img in images_files:
 
     # Inicializar el descriptor AKAZE y detectar keypoints y extraer los descriptores de la imagen.
     # kpts -> keypoints
-    # descs -> descriptores   
+    # descs -> descriptores
     kpts, descs = akaze.detectAndCompute(img_g, None)
     print("keypoints: {}, descriptors: {}".format(len(kpts), descs.shape))
 
@@ -88,13 +98,17 @@ for img in images_files:
 # Utilizamos distancia de Hamming porque AKAZE es un descriptor binario
 matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_BRUTEFORCE_HAMMING)
 
-for (i, img_i) in enumerate(list(images.values())):
-    for (j, img_j) in enumerate(list(images.values())[i+1::]):
+for i, img_i in enumerate(list(images.values())):
+    for j, img_j in enumerate(list(images.values())[i + 1 : :]):
         # Encontrar los match
-        matches = matchWithRatioTest(matcher, img_i.descriptors, img_j.descriptors)
+        matches = matchWithRatioTest(
+            matcher, img_i.descriptors, img_j.descriptors
+        )
 
         # Test de reciprocidad
-        matchRcp = matchWithRatioTest(matcher, img_j.descriptors, img_i.descriptors)
+        matchRcp = matchWithRatioTest(
+            matcher, img_j.descriptors, img_i.descriptors
+        )
 
         merged_matches = []
 
@@ -102,13 +116,16 @@ for (i, img_i) in enumerate(list(images.values())):
             found = False
             for m_m in matches:
                 # Solo se acepta el match si es recíproco
-                if(mR_m.queryIdx == m_m.trainIdx and mR_m.trainIdx == m_m.queryIdx):
+                if (
+                    mR_m.queryIdx == m_m.trainIdx
+                    and mR_m.trainIdx == m_m.queryIdx
+                ):
                     merged_matches.append(m_m)
                     found = True
                     break
                 if found:
                     continue
-        
+
         # Restricción Epipolar. Calculamos la matriz fundamental utilizando RANSAC
         img_i_pts = []
         img_j_pts = []
@@ -116,21 +133,26 @@ for (i, img_i) in enumerate(list(images.values())):
         for m in merged_matches:
             img_i_pts.append(img_i.keypoints[m.queryIdx].pt)
             img_j_pts.append(img_j.keypoints[m.trainIdx].pt)
-        
-        F, inliersMask = cv.findFundamentalMat(np.array([img_i_pts]), np.array([img_j_pts]), cv.FM_RANSAC)
+
+        F, inliersMask = cv.findFundamentalMat(
+            np.array([img_i_pts]), np.array([img_j_pts]), cv.FM_RANSAC
+        )
 
         final = []
 
-
-        for (mask, match) in zip(inliersMask, merged_matches):
+        for mask, match in zip(inliersMask, merged_matches):
             if mask:
                 final.append(match)
-        
-        if (float(len(final)) / float(len(matches)) < PAIR_MATCH_SURVIVAL_RATE):
-            print("El match final tiene menos de la mitad de matches que el original. Ignorando imagen.")
+
+        if float(len(final)) / float(len(matches)) < PAIR_MATCH_SURVIVAL_RATE:
+            print(
+                "El match final tiene menos de la mitad de matches que el original. Ignorando imagen."
+            )
             continue
-        
-        images[img_i.img_name].matches[(img_i.img_name,img_j.img_name)] = final
+
+        images[img_i.img_name].matches[
+            (img_i.img_name, img_j.img_name)
+        ] = final
 
 # Crear grafo de características para rastrearlas
 
@@ -143,7 +165,7 @@ for img_tpl in images.values():
     for i in range(len(img_tpl.keypoints)):
         node = ImageFeature(img_tpl.img_name, i)
         G.add_node(node)
-        vertexByImageFeature[(img_tpl.img_name,i)] = node
+        vertexByImageFeature[(img_tpl.img_name, i)] = node
 
 # Agregamos las aristas (matches)
 for img_tpl in images.values():
@@ -165,7 +187,7 @@ for c in components:
     imagesInComponent = set()
     for img in c:
         img_name = img.img_name
-        if(img_name in imagesInComponent):
+        if img_name in imagesInComponent:
             # La imagen ya se encuentra en el componente
             isGoodComponent = False
             break
@@ -176,4 +198,3 @@ for c in components:
 
 print(f"Total de componentes: {nx.number_connected_components(G)}")
 print(f"Componentes buenos: {len(goodComponents)}")
-
--- matcher.py
+++ matcher.py
@@ -1,9 +1,12 @@
 import cv2 as cv
 
-def matchWithRatioTest(matcher:cv.DescriptorMatcher, desc1, desc2, nn_match_ratio_th = 0.8):
+
+def matchWithRatioTest(
+    matcher: cv.DescriptorMatcher, desc1, desc2, nn_match_ratio_th=0.8
+):
     nn_matches = matcher.knnMatch(desc1, desc2, 2)
     ratioMatched = []
-    for m,n in nn_matches:
+    for m, n in nn_matches:
         if m.distance < n.distance * nn_match_ratio_th:
             ratioMatched.append(m)
 
